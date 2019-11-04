a603130f07819ac7420b3833eb35829ecc96a85a
Brian Nelson <ladofmusi@outlook.com>
2019-11-04 00:23:48 -0700

    got a v-card up and running

--- finalv4/src/main.js
+++ finalv4/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import axios from 'axios';
+
+
 
 Vue.config.productionTip = false
 
@@ -10,5 +13,19 @@ new Vue({
   router,
   store,
   vuetify,
+
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  },
+  
   render: h => h(App)
+  
+
 }).$mount('#app')
--- finalv4/src/views/About.vue
+++ finalv4/src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<v-container>
+  <v-row>
+
+<v-col cols='3' v-for="ship in starships" :key="ship.model">
+<v-card>
+  <v-card-title>
+    <div>
+      <h3>
+        {{ ship.name }}
+      </h3>
+      <div> {{ ship.model }} </div>
+      <p> {{ ship.manufacturer }} </p>
+    
+    </div>
+  </v-card-title>
+  <v-card-action>
+    <v-btn @click="showing = false">hide</v-btn>
+  </v-card-action>
+</v-card>
+</v-col>
+
+  </v-row>
+</v-container>
 </template>
+
+<script>
+import { aboutMixin } from './aboutmixintest'
+
+export default {
+  data() { return 
+    showing: false
+  },
+  mixins: [aboutMixin]
+  
+}
+</script>
